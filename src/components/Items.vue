5aec6ec20840ac89ac3af0efdd5b8f3372e361fa
JaytheKK <yannik.fischer@student.htldornbirn.at>
2021-12-07 09:59:02 +0100

    init

---
+++ src/components/Items.vue
@@ -0,0 +1,191 @@
+<template>
+<div class="items_Wrapper">
+    <div class="item_list">
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 1</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 2</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 3</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+    </div>
+    <div class="item_list">
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 4</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 5</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 6</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 7</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+    </div>
+    <div class="item_list">
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 8</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 9</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 10</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+    </div>
+    <div class="item_list">
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 11</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 12</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 13</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item_img">
+          <img src="../assets/Anton.jpg" alt="item_picture" class="img_responsive">
+        </div>
+        <h1>Item 14</h1>
+        <div class="description">
+          <p>this is a description to this item</p>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Items',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+.items_Wrapper {
+  margin-top: 100px;
+  height: auto;
+}
+
+.item_list {
+  display: flex;
+
+}
+
+.item {
+  width: 25%;
+  text-align: center;
+  margin: 30px;
+  cursor: pointer;
+  border-radius: 16px;
+
+  background-color: grey;
+}
+
+.item h1 {
+  font-size: 45px;
+  margin: auto;
+}
+
+.item_img {
+  width: 100%;
+  transition: all 0.2s ease-in-out;
+  margin-top: 10px;
+}
+
+.item_img img {
+  transition: all 0.2s ease-in-out;
+  border-radius:16px;
+  width: 75%;
+}
+
+.item:hover .item_img img {
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.20);
+}
+ 
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+<nav class="navbar">
+    <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+    </div>
+    <button class="searchButton">
+        <img
+        src="../assets/searchIconTransparent.png"
+        alt="Image not Found"
+        class="searchIcon"
+        />
+    </button>
+    <button class="lost">Lost</button>
+    <button class="found">Found</button>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+.navbar {
+  width: 100%;
+  background-color: #313131;
+  margin: auto;
+  position: fixed;
+  top: 0;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+}
+
+.search-box .search-bar {
+  display: block;
+  padding: 20px;
+  height: 31px;
+  color: #313131;
+  font-size: 25px;
+  -webkit-appearance: none;
+     -moz-appearance: none;
+          appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.75);
+}
+
+button {
+  width: 250px;
+  background-color: rgba(255, 255, 255, 0.75);
+  margin-left: 5px;
+  border: none;
+  outline: none;
+  font-size: 35px;
+  font-weight: bold;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+ "Lucida Sans", Arial, sans-serif;
+}
+
+.lost {
+  margin-left: auto;
+}
+
+button:hover {
+  background-color: rgba(255, 255, 255, 0.65);
+  -webkit-transition: 0.3s ease;
+  transition: 0.3s ease;
+}
+
+.searchIcon {
+  height: 69px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  margin: auto;
+}
+
+button.searchButton {
+  width: 100px;
+  margin: 0;
+  padding: 0;
+  display: center;
+}
+</style>
